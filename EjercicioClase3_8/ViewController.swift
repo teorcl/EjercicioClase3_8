e4353c129e345788610dbf9292428a29e98d408c
Teodoro <teo.calle.lara@gmail.com>
2022-11-08 19:58:26 -0500

    Initial Commit

---
+++ EjercicioClase3_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EjercicioClase3_8
+//
+//  Created by TEO on 8/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
